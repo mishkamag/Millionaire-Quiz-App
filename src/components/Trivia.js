711b042b4e3b609d15a25852af71ad0d99211ef6
mishkamag <m93.maglaperidze@gmail.com>
2022-10-25 00:32:59 +0400

    add question feature

---
+++ src/components/Trivia.js
@@ -0,0 +1,15 @@
+import "./trivia.css";
+
+export default function Trivia() {
+  return (
+    <div className="trivia">
+      <div className="question">რომელ წელს მოხდა დიდგორის ბრძოლა?</div>
+      <div className="answers">
+        <div className="answer">1212</div>
+        <div className="answer">1121</div>
+        <div className="answer">1122</div>
+        <div className="answer">1221</div>
+      </div>
+    </div>
+  );
+}
