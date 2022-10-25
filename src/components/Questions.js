509e55f3b7acdf18447cbaf4b9849bc73ea02a08
mishkamag <m93.maglaperidze@gmail.com>
2022-10-25 23:01:51 +0400

    add some dummy questions data

---
+++ src/components/Questions.js
@@ -0,0 +1,70 @@
+const data = [
+  {
+    id: 1,
+    question: "რომელ წელს მოხდა დიდგორის ბრძოლა?",
+    answers: [
+      {
+        text: "1122",
+        correct: false,
+      },
+      {
+        text: "1121",
+        correct: true,
+      },
+      {
+        text: "1112",
+        correct: false,
+      },
+      {
+        text: "1221",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "ურემი რომ გადაბრუნდება გზა მერე ...?",
+    answers: [
+      {
+        text: "გამოჩნდებაო",
+        correct: true,
+      },
+      {
+        text: "გაჩნდებაო",
+        correct: false,
+      },
+      {
+        text: "გახდებაო",
+        correct: false,
+      },
+      {
+        text: "გასწორდებაო",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "ვინ მოკლა ძერა?",
+    answers: [
+      {
+        text: "ჯონი დეპმა",
+        correct: false,
+      },
+      {
+        text: "სხვა ძერამ",
+        correct: false,
+      },
+      {
+        text: "არ მომკვდარა",
+        correct: false,
+      },
+      {
+        text: "შოთამ",
+        correct: true,
+      },
+    ],
+  },
+];
+
+export default data;
