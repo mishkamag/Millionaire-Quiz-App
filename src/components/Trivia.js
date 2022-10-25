1a45329f6af14af1a4e92278f9d037c6087c1a17
mishkamag <m93.maglaperidze@gmail.com>
2022-10-26 00:35:37 +0400

    click answer

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -6,17 +6,29 @@ export default function Trivia(props) {
   const { setTimeOut, questionNumber, setQuestionNumber } = props;
 
   const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [className, setClassName] = useState("answer");
 
   useEffect(() => {
     setQuestion(data[questionNumber - 1]);
   }, [data, questionNumber]);
+
+  const clickHandler = (a) => {
+    setSelectedAnswer(a);
+    setClassName("answer active");
+  };
 
   return (
     <div className="trivia">
       <div className="question">{question?.question}</div>
       <div className="answers">
         {question?.answers.map((a) => (
-          <div className="answer ">{a.text}</div>
+          <div
+            className={selectedAnswer === a ? className : "answer"}
+            onClick={() => clickHandler(a)}
+          >
+            {a.text}
+          </div>
         ))}
       </div>
     </div>
